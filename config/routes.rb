b2e8fa153c30013254318ba17e679400bb2b37cf
Ophira Blumner <ophirablumner@gmail.com>
2019-11-21 16:53:07 +0200

    on my way to heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'cocktails#index'
+  resources :cocktails, only: %i[index show new create] do
+    resources :doses, only: %i[new create]
+  end
+  resources :doses, only: %i[destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
